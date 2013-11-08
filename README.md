5b06d6ddbb5f4e2b792ad0a2bcd931befec3b8a5
Philipp Dunkel <p.dunkel@durchblicker.at>
2013-11-08 09:59:06 +0100

    Initial Commit of Greatness ;)

--- README.md
+++ README.md
@@ -2,3 +2,17 @@ bunyan-mqtt
 ===========
 
 A bunyan raw stream to publish to mqtt
+
+## Install
+
+    npm install bunyan-mqtt
+
+## Use
+
+    var BunyanMqtt = require('bunyan-mqtt');
+    var bunyan = require('bunyan');
+    var log = bunyan.createLogger({
+      streams:[
+        { level:'info', type:'raw', stream:bmqtt({ topic:'logging', port:1873, host:'localhost' }) }
+      ]
+    });
---
+++ bunyan-mqtt.js
@@ -0,0 +1,21 @@
+/*
+** © 2013 by Philipp Dunkel <pip@pipobscure.com>. Licensed under MIT-License.
+*/
+
+'use strict';
+
+exports = module.exports = createStream;
+
+var mqtt = require('mqtt');
+
+function createStream(opts) {
+  return Object.create(opts || {}, {
+    write:{ value:write, enumerable:true, configurable:true, writable:true },
+    mqtt:{ value:opts.mqtt || mqtt.createClient(opts.host || 1883, opts.port || 'localhost'), enumerable:false, configurable:false, writable:false }
+  });
+}
+
+function write(item) {
+  /*jslint validthis:true */
+  this.mqtt.publish(this.topic || 'bunyan-mqtt', JSON.stringify(item));
+}
