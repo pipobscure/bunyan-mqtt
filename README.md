906b67649bb75d865b6a6be97cef6aafce567837
Philipp Dunkel <p.dunkel@durchblicker.at>
2013-11-12 21:50:03 +0100

    add level to topic (optional)

--- README.md
+++ README.md
@@ -23,5 +23,6 @@ A bunyan raw stream to publish to mqtt
  * **host** - host to use as broker *(default: localhost)*
  * **port** - the port to use on the broker *(default: 1883)*
  * **topic** - the topic to use for publishing *(default: bunyan)*
+ * **addLevel** - whether to add '/&lt;level&gt;' to the topic. *(default: false)*
  * **qos** - the numeric *qos* to use in mqtt
  * **retain** - whether the broker should retain messages
--- bunyan-mqtt.js
+++ bunyan-mqtt.js
@@ -9,14 +9,27 @@ exports = module.exports = createStream;
 var mqtt = require('mqtt');
 
 function createStream(opts) {
-  console.error(opts);
+  opts.topic = opts.topic || 'bunyan';
+  opts.addLevel = !!opts.addLevel;
   return Object.create(opts || {}, {
-    write:{ value:write, enumerable:true, configurable:true, writable:true },
-    mqtt:{ value:opts.mqtt || mqtt.createClient(opts.port || 1883, opts.host || 'localhost', opts), enumerable:false, configurable:false, writable:false }
+    write:{
+      value:write,
+      enumerable:true,
+      configurable:true,
+      writable:true
+    },
+    mqtt:{
+      value:opts.mqtt || mqtt.createClient(opts.port || 1883, opts.host || 'localhost', opts),
+      enumerable:false,
+      configurable:false,
+      writable:false
+    }
   });
 }
 
 function write(item) {
   /*jslint validthis:true */
-  this.mqtt.publish(this.topic || 'bunyan', JSON.stringify(item), this);
+  var topic = [ this.topic ];
+  if (this.addLevel) topic.push(item.level || '100');
+  this.mqtt.publish(topic.join('/'), JSON.stringify(item), this);
 }
